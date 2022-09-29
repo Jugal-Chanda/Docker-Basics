748b1910f82d1acccecba70bfe6e42ee2bd60a64
BS753 <jugalchanda7@gmail.com>
2022-09-29 19:32:29 +0600

    remove mysql and dynamic config file

--- moodle_docker/moodle400/config.php
+++ moodle_docker/moodle400/config.php
@@ -38,12 +38,12 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'mysqli';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
+$CFG->dbtype    = getenv('DB_TYPE');      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
 $CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'moodledb';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'root';   // your database username
-$CFG->dbpass    = 'bs23';   // your database password
+$CFG->dbhost    = getenv('DB_HOST');  // eg 'localhost' or 'db.isp.com' or IP
+$CFG->dbname    = getenv('DB_NAME');     // database name, eg moodle
+$CFG->dbuser    = getenv('DB_USER');   // your database username
+$CFG->dbpass    = getenv('DB_PASS');   // your database password
 $CFG->prefix    = 'mdl_';       // prefix to use for all table names
 $CFG->dboptions = array(
     'dbpersist' => false,       // should persistent database connections be
@@ -56,7 +56,7 @@ $CFG->dboptions = array(
                                 //  (please note mysql is always using socket
                                 //  if dbhost is 'localhost' - if you need
                                 //  local port connection use '127.0.0.1')
-    'dbport'    => '',          // the TCP port number to use when connecting
+    'dbport'    => getenv('DB_PORT'),          // the TCP port number to use when connecting
                                 //  to the server. keep empty string for the
                                 //  default port
     'dbhandlesoptions' => false,// On PostgreSQL poolers like pgbouncer don't
@@ -131,8 +131,8 @@ $CFG->dboptions = array(
 //
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
-
-$CFG->wwwroot   = 'http://localhost:8008';
+$app_port = getenv('APP_PORT');
+$CFG->wwwroot   = 'http://localhost:'.$app_port;
 
 
 //=========================================================================
